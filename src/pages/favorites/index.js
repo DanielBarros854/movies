b6205be1fdabb9cd59d3843c6b4631617a46e037
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-04-29 11:21:43 -0300

    feat: implement saved movies

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import './favorites.css'
+
 const Favorites = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+
+    const my_list = localStorage.getItem('movies');
+    setMovies(JSON.parse(my_list) || []);
+
+  }, [])
+
+  const handleDelete = (movie_id) => {
+    let filter_movies = movies.filter((movie) => {
+      return (movie.id !== movie_id)
+    });
+
+    setMovies(filter_movies);
+    localStorage.setItem('movies', JSON.stringify(filter_movies));
+  }
+
   return (
-    <div>
-      <h1>Favoritos</h1>
+    <div id="my-movies">
+      <h1>Meus Filmes</h1>
+
+      <ul>
+        {movies.map((movie) => {
+          return (
+            <li>
+              <span>{movie.nome}</span>
+
+              <div className="buttons">
+                <Link to={`/filme/${movie.id}`}>Ver detalhes</Link>
+                <button onClick={ () => handleDelete(movie.id) }>Excluir</button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 };
